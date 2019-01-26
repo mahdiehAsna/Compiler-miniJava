a7c94de2f5133e76826fef2d48d5ef72062cd830
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2019-01-26 16:37:12 +0330

    Added PrintListener Class

---
+++ MinijavaPrintListener.py
@@ -0,0 +1,26 @@
+from grammer import MinijavaListener, MinijavaParser
+
+
+class MiniJavaPrintListener(MinijavaListener):
+    def __init__(self, name):
+        super(MinijavaListener, self).__init__()
+        self.code = ""
+        self.name = name
+        self.block_number = 0
+        self.variables = {}
+
+    def enterGoal(self, ctx: MinijavaParser.GoalContext):
+        self.code += '.class public %s' % self.name + '\n'
+        self.code += '.super java/lang/Object' + '\n'
+        self.code += '.method public <init>()V' + '\n'
+        self.code += 'aload_0' + '\n'
+        self.code += 'invokenonvirtual java/lang/Object/<init>()V' + '\n'
+        self.code += 'return' + '\n'
+        self.code += '.end method' + '\n'
+        self.code += '.method public static main([Ljava/lang/String;)V' + '\n'
+        self.code += '.limit stack 10000' + '\n'
+
+    def exitGoal(self, ctx:MinijavaParser.GoalContext):
+        self.code += "return" + '\n'
+        self.code += ".end method" + '\n'
+
